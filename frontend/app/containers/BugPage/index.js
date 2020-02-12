a6ba5705b53f58a77a3c94c355fb7b137c14a25c
Kevin Tatou <kevintatou94@gmail.com>
2020-02-12 18:47:37 +0100

    Hello World in React - Added CenteredSection and Section to BugPage - Added hello world message to messages - Added message to index

--- frontend/app/containers/BugPage/index.js
+++ frontend/app/containers/BugPage/index.js
@@ -7,11 +7,10 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 
-import H1 from 'components/H1';
+import H2 from 'components/H2';
 import messages from './messages';
-import List from './List';
-import ListItem from './ListItem';
-import ListItemTitle from './ListItemTitle';
+import CenteredSection from './CenteredSection';
+
 
 export default function BugPage() {
 
@@ -24,6 +23,15 @@ export default function BugPage() {
           content="Bug page of React.js Boilerplate application"
         />
       </Helmet>
+
+      <div>
+      <CenteredSection>
+          <H2>
+            <FormattedMessage {...messages.helloWorld} />
+          </H2>
+
+        </CenteredSection>
+      </div>
      
 
     </div>
